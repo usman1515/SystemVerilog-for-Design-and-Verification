45649e0488f3277ec7167a37d74da6f6b86b40a4
Usman Siddique <usmansiddique1515@gmail.com>
2022-02-25 01:14:05 +0500

    created dut of lab 1

---
+++ lab_1/register.sv
@@ -0,0 +1,21 @@
+timeunit 1ns;
+timeprecision 100ps;
+
+module register #(parameter DATA_WIDTH=8) (
+    input   logic   clk,
+    input   logic   rst_,
+    input   logic   en,
+    input   logic   [DATA_WIDTH-1:0]  data,
+    output  logic   [DATA_WIDTH-1:0]  out
+);
+
+always_ff @(posedge clk, negedge rst_) begin
+    if (rst_) begin
+        if(en)
+			out = data;
+    end
+    else
+        out = 'b0;
+end
+
+endmodule
