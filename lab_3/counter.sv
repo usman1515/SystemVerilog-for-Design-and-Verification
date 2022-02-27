031cd54375882de4480627546b00d584070a4b22
Usman Siddique <usmansiddique1515@gmail.com>
2022-02-27 17:35:48 +0500

    created dut of lab 3

---
+++ lab_3/counter.sv
@@ -0,0 +1,27 @@
+timeunit 1ns;
+timeprecision 100ps;
+
+module counter #(parameter DATA_WIDTH = 5)(
+    input   logic                   clk ,
+    input   logic                   rst_,
+    input   logic                   enable,
+    input   logic                   load,
+    input   logic [DATA_WIDTH-1:0]  data,
+    output  logic [DATA_WIDTH-1:0]  count
+);
+
+always_ff @(posedge clk, negedge rst_) begin
+    if (rst_) begin
+        if(load) begin
+            count <= data;
+        end
+        else if (enable) begin
+            count <= count + 1;
+        end
+    end
+    else begin
+        count <= 'd0;
+    end
+end
+
+endmodule
