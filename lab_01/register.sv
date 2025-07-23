4a01899afddd6f6679550290d73c10e12680ffe3
usman1515 <usmansiddique1515@gmail.com>
2025-07-23 13:39:15 +0200

    refactor: lab 01 rtl

--- lab_01/register.sv
+++ lab_01/register.sv
@@ -1,20 +1,22 @@
 `timescale 1ns/100ps
 
 module register #(parameter DATA_WIDTH=8) (
-    input   logic   clk,
-    input   logic   rst_,
-    input   logic   en,
-    input   logic   [DATA_WIDTH-1:0]  data,
-    output  logic   [DATA_WIDTH-1:0]  out
+    input logic     clk,
+    input logic     rst_n,
+    input logic     i_en,
+    input logic     [DATA_WIDTH-1:0] i_data,
+    output logic    [DATA_WIDTH-1:0] o_data
 );
 
-always_ff @(posedge clk, negedge rst_) begin
-    if (rst_) begin
-        if(en)
-			out = data;
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (rst_n) begin
+            if(i_en)
+                o_data <= i_data;
+        end
+        else begin
+            o_data <= '0;
+        end
     end
-    else
-        out = 'b0;
-end
 
 endmodule
+
