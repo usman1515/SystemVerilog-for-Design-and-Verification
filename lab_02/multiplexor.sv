df5164b0cc06396e8401c2f6e5914b3e2090b7b0
usman1515 <usmansiddique1515@gmail.com>
2025-07-23 13:59:53 +0200

    refactor: lab 02 rtl

--- lab_02/multiplexor.sv
+++ lab_02/multiplexor.sv
@@ -1,18 +1,19 @@
 `timescale 1ns/100ps
 
 module multiplexor #(parameter DATA_WIDTH = 4) (
-    input   logic [DATA_WIDTH-1:0]  in_a,
-    input   logic [DATA_WIDTH-1:0]  in_b,
-    input   logic                   sel,
-    output  logic [DATA_WIDTH-1:0]  out
+    input logic     [DATA_WIDTH-1:0] i_a,
+    input logic     [DATA_WIDTH-1:0] i_b,
+    input logic     i_sel,
+    output logic    [DATA_WIDTH-1:0] o_data
 );
 
 always_comb begin
-    unique case (sel)
-        'd0 : out = in_b;
-        'd1 : out = in_a; 
-        default: out = '0;
+    unique case (i_sel)
+        'd0 : o_data = i_b;
+        'd1 : o_data = i_a;
+        default: o_data = '0;
     endcase
 end
 
 endmodule
+
