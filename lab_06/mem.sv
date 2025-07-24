908e9a858802fdf3f8b3c58a095d55eadd73e3fa
usman1515 <usmansiddique1515@gmail.com>
2025-07-24 16:43:11 +0200

    refactor: lab 06 rtl

--- lab_06/mem.sv
+++ lab_06/mem.sv
@@ -1,21 +1,26 @@
 `timescale 1ns/100ps
 
-module mem #(parameter ADDR_WIDTH=5, parameter DATA_WIDTH=8) (
-    input   logic                   clk,
-    input   logic                   read,
-    input   logic                   write,
-    input   logic [ADDR_WIDTH-1:0]  addr,
-    input   logic [DATA_WIDTH-1:0]  data_in ,
-    output  logic [DATA_WIDTH-1:0]  data_out
+`define ADDR_WIDTH 5
+`define DATA_WIDTH 8
+
+module mem (
+    input logic     clk,
+    input logic     i_read,
+    input logic     i_write,
+    input logic     [`ADDR_WIDTH-1:0] i_addr,
+    input logic     [`DATA_WIDTH-1:0] i_data,
+    output logic    [`DATA_WIDTH-1:0] o_data
 );
 
-    logic [0:DATA_WIDTH-1] [(2**ADDR_WIDTH)-1:0] mem_block ;
+    // logic [`DATA_WIDTH-1 : 0] mem_block [0 : (2**ADDR_WIDTH)-1];
+    logic [`DATA_WIDTH-1 : 0] mem_block [(2**`ADDR_WIDTH)-1];
 
     always_ff @(posedge clk) begin
-        if(write && !read)
-            mem_block[addr] <= data_in;
-        if(read && !write)
-            data_out <= mem_block[addr];
+        if(i_write && !i_read)
+            mem_block[i_addr] <= i_data;
+        if(i_read && !i_write)
+            o_data <= mem_block[i_addr];
     end
 
 endmodule
+
